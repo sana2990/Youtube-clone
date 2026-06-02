326a6e182b29082903c3c8260b370d617d43efe2
sana2990 <sanaasad2990@gmail.com>
2026-06-02 12:09:52 +0530

    Add videos javascript

---
+++ youtube/src/utils/videos.js
@@ -0,0 +1,46 @@
+const videos = [
+  {
+    id: 1,
+    title: "React JS Full Course for Beginners",
+    thumbnail: "https://i.ytimg.com/vi/bMknfKXIFA8/hqdefault.jpg",
+    channel: "freeCodeCamp",
+    views: "4.8M views",
+  },
+  {
+    id: 2,
+    title: "JavaScript Crash Course",
+    thumbnail: "https://i.ytimg.com/vi/hdI2bqOjy3c/hqdefault.jpg",
+    channel: "Traversy Media",
+    views: "2.1M views",
+  },
+  {
+    id: 3,
+    title: "Build a YouTube Clone with React",
+    thumbnail: "https://i.ytimg.com/vi/FHTbsZEJspU/hqdefault.jpg",
+    channel: "CodeWithHarry",
+    views: "850K views",
+  },
+  {
+    id: 4,
+    title: "Node.js Tutorial for Beginners",
+    thumbnail: "https://i.ytimg.com/vi/TlB_eWDSMt4/hqdefault.jpg",
+    channel: "Programming with Mosh",
+    views: "3.2M views",
+  },
+  {
+    id: 5,
+    title: "Learn CSS Grid in 20 Minutes",
+    thumbnail: "https://i.ytimg.com/vi/9zBsdzdE4sM/hqdefault.jpg",
+    channel: "Web Dev Simplified",
+    views: "1.4M views",
+  },
+  {
+    id: 6,
+    title: "React Project Tutorial",
+    thumbnail: "https://i.ytimg.com/vi/w7ejDZ8SWv8/hqdefault.jpg",
+    channel: "Traversy Media",
+    views: "1.9M views",
+  },
+];
+
+export default videos;
